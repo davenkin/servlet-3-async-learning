e366e2356fa3f2cc5febf74607bae56b025dbefd
Teng Yun <davenkin@gmail.com>
2017-03-13 20:13:30 +0800

    Teng Yun:init project

---
+++ src/main/java/davenkin/servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package davenkin.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.write("Hello World!");
+        out.close();
+    }
+
+}
