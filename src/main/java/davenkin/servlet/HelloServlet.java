5007009747baf678fd9df1f6c60715f180b50dae
Teng Yun <davenkin@gmail.com>
2017-03-14 10:14:08 +0800

    step 1: synced helloworld

--- src/main/java/davenkin/servlet/HelloServlet.java
+++ src/main/java/davenkin/servlet/HelloServlet.java
@@ -7,16 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.concurrent.ThreadLocalRandom;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws ServletException, IOException {
+        sleepForUpToTwoSeconds();
         PrintWriter out = response.getWriter();
         out.write("Hello World!");
         out.close();
+    }
+
+    private void sleepForUpToTwoSeconds() {
+        try {
+            Thread.sleep(ThreadLocalRandom.current().nextInt(2000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
